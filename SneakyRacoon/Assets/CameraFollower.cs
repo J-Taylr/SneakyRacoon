6c7b11ebea0e75382ba1d78bb03f46a583a33283
J-Taylor <jmarcustaylor@gmail.com>
2021-01-30 11:12:39 +0000

    alternate camera option, follow player

---
+++ SneakyRacoon/Assets/CameraFollower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    GameObject player;
+
+    public float CamZOffset;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    public void FollowPlayer()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z + CamZOffset));
+    }
+
+}
