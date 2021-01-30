ced3e2f88a583c9fd55d44e1765e6443668f561f
J-Taylor <jmarcustaylor@gmail.com>
2021-01-30 13:14:15 +0000

    editing table script

---
+++ SneakyRacoon/Assets/HidingSpot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingSpot : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player")) // if player is under a table
+        {
+            print("collidah");
+            Color tablecolor = gameObject.GetComponent<SpriteRenderer>().color;
+            tablecolor.a = 0.5f;
+            gameObject.GetComponent<SpriteRenderer>().color = tablecolor; //these lines change the alpha level of the table when the player is under it 
+
+            GameManager.Instance.PlayerisHidden = true;
+        }
+    }
+
+
+    public void HideOn()
+    {
+        
+         
+        Color tablecolor = gameObject.GetComponent<SpriteRenderer>().color;
+        tablecolor.a = 0.5f;
+        gameObject.GetComponent<SpriteRenderer>().color = tablecolor; //these lines change the alpha level of the table when the player is under it
+    }
+
+}
--- SneakyRacoon/Assets/Scripts/PlayerManager.cs
+++ SneakyRacoon/Assets/Scripts/PlayerManager.cs
@@ -15,31 +15,12 @@ public class PlayerManager : MonoBehaviour
             GameManager.Instance.playerScore++;
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("Hiding")) // if player is under a table
-        {
-            Color tablecolor = collision.gameObject.GetComponent<SpriteRenderer>().color;
-            tablecolor.a = 0.5f;
-            collision.gameObject.GetComponent<SpriteRenderer>().color = tablecolor; //these lines change the alpha level of the table when the player is under it 
-
-            GameManager.Instance.PlayerisHidden = true;
-        }
+        
 
         
     }
 
 
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Hiding")) // if player leaves table
-        {
-            Color tablecolor = collision.gameObject.GetComponent<SpriteRenderer>().color;
-            tablecolor.a = 1;
-            collision.gameObject.GetComponent<SpriteRenderer>().color = tablecolor; // change the alpha back to 100 when the player leaves
-
-
-            GameManager.Instance.PlayerisHidden = false;
-
-        }
-    }
+  
 }
